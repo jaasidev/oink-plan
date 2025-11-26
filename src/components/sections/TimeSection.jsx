d54a9ce50cc78721fed1c60111d697831bfbf566
Jaasiel Beltran <jaasiel0303@gmail.com>
2025-11-26 07:04:16 -0500

    fix: mejorando aspecto visual en el modo claro

--- src/components/sections/TimeSection.jsx
+++ src/components/sections/TimeSection.jsx
@@ -9,7 +9,7 @@ export function TimeSection() {
       <h2 className='text-center font-bold text-2xl mb-3 mt-12'>
         Selector de tiempo
       </h2>
-      <div className='flex flex-col lg:flex-row rounded-2xl lg:items-center bg-base-200'>
+      <div className='flex flex-col lg:flex-row rounded-2xl lg:items-center bg-base-200 shadow-sm'>
         <div className=' flex overflow-y-auto gap-1 px-3 py-4 lg:w-3/4 order-2 lg:order-1 '>
           {time.map((value, index) => {
             return (
