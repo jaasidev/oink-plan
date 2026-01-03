df0dc50bb63740e011ad471a40477fbcf8cd64b3
Jaasiel Beltrán <jaasiel0303@gmail.com>
2026-01-03 16:16:15 -0500

    fix: arreglando update en funcion para fecha is bloque

--- src/utils/droptime.js
+++ src/utils/droptime.js
@@ -1,5 +1,6 @@
 function isBloque(date, multiplicador, i = 0, condicion) {
-  if (multiplicador === 1) return (date - i * multiplicador) % 5 == 0 ? true : false
+  if (multiplicador === 1)
+    return (date - i * multiplicador) % 5 == 0 ? true : false
   else {
     return condicion % 5 == 0 ? true : false
   }
@@ -9,7 +10,6 @@ function formatDateString(date) {
   return String(date).padStart(2, '0')
 }
 
-
 export function time(multiplicador) {
   const date = new Date()
   const array = []
@@ -17,17 +17,23 @@ export function time(multiplicador) {
     for (let i = 0; i < 60 / multiplicador; i++) {
       if (date.getMinutes() >= i * multiplicador) {
         array.unshift({
-          hours: formatDateString(date.getHours() - j) >= 0 ? (formatDateString(date.getHours() - j)) : (formatDateString(date.getHours() - j + 24)),
+          hours:
+            formatDateString(date.getHours() - j) >= 0
+              ? formatDateString(date.getHours() - j)
+              : formatDateString(date.getHours() - j + 24),
           minutes: formatDateString(date.getMinutes() - i * multiplicador),
           color: 'none',
-          bloque: isBloque(date.getMinutes(), multiplicador, i, array.length)
+          bloque: isBloque(date.getMinutes(), multiplicador, i, array.length),
         })
       } else {
         array.unshift({
-          hours: formatDateString(date.getHours() - j - 1) >= 0 ? formatDateString(date.getHours() - j - 1) : formatDateString(date.getHours() + 23 - j),
+          hours:
+            formatDateString(date.getHours() - j - 1) >= 0
+              ? formatDateString(date.getHours() - j - 1)
+              : formatDateString(date.getHours() + 23 - j),
           minutes: formatDateString(60 + date.getMinutes() - i * multiplicador),
           color: 'none',
-          bloque: isBloque(date.getMinutes(), multiplicador, i, array.length)
+          bloque: isBloque(date.getMinutes(), multiplicador, i, array.length),
         })
       }
     }
@@ -39,13 +45,18 @@ export function addDate(array, multiplicador) {
   if (array.length > 0) {
     const newArray = [...array]
     const date = new Date()
-    const lastBloque = newArray.findLastIndex(value => value.bloque == true)
+    const lastBloque = newArray.findLastIndex((value) => value.bloque == true)
     newArray.push({
       hours: formatDateString(date.getHours()),
       minutes: formatDateString(date.getMinutes()),
       color: 'none',
       disable: 'disabled',
-      bloque: isBloque(date.getMinutes(), multiplicador, newArray.length - 1 - lastBloque)
+      bloque: isBloque(
+        date.getMinutes(),
+        multiplicador,
+        0,
+        newArray.length - 1 - lastBloque
+      ),
     })
     newArray.shift()
     sessionStorage.setItem('prev', JSON.stringify(newArray))
