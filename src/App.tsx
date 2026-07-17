5758b3bcc39af2f26dc4ba654fdada0e7f4f81da
Jaasiel Beltrán <jaasiel0303@gmail.com>
2026-07-17 18:46:41 -0400

    fix: separando el contador de velas y su implementacion de el estado final

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App() {
   const setTime = useTimeStore((state) => state.setTime)
   const setContador = useTimeStore((state) => state.setContador)
   const setEstrategias = useTimeStore((state) => state.setEstrategias)
-  const updateVelas = useTimeStore((state) => state.updateVelas)
 
   useEffect(() => {
     const previousTime = sessionStorage.getItem('prev')
@@ -18,7 +17,6 @@ function App() {
       setTime(JSON.parse(previousTime))
       setContador(Number.parseInt(previousContador) as Bloque)
       setEstrategias()
-      updateVelas()
     }
   }, [])
   return (
--- src/context/useTimeStore.ts
+++ src/context/useTimeStore.ts
@@ -1,27 +1,16 @@
 import { create } from 'zustand'
 import { estrategia } from '../utils/estrategias'
 import { addDate, initTime } from '../utils/droptime'
-import { ApuestasEstados } from '../schemas/enums'
 import type { Tiempo, EstrategiaBasica, Bloque } from '../schemas/estrategia'
-
-interface Velas {
-  total: number
-  high: number
-  stage: number
-  low: number
-}
 
 interface StoreProps {
   time: Tiempo[]
   contador: Bloque
   estrategias: EstrategiaBasica[]
-  velas: Velas
   setTime: (value: Tiempo[]) => void
   setContador: (value: Bloque) => void
   setEstrategias: () => void
   resetEstrategias: () => void
-  updateVelas: () => void
-  resetVelas: () => void
   updateTime: () => void
   resetTime: () => void
   setInitTime: () => void
@@ -31,12 +20,6 @@ export const useTimeStore = create<StoreProps>((set) => ({
   time: [],
   contador: 1,
   estrategias: [],
-  velas: {
-    total: 0,
-    high: 0,
-    stage: 0,
-    low: 0,
-  },
   setTime: (value) => {
     set({ time: value })
   },
@@ -57,32 +40,5 @@ export const useTimeStore = create<StoreProps>((set) => ({
   },
   resetEstrategias: () => {
     set(() => ({ estrategias: [] }))
-  },
-  updateVelas: () => {
-    set((state) => ({
-      velas: state.time.reduce(
-        (acc, value) => {
-          if (value.color !== ApuestasEstados.NONE) acc.total += 1
-
-          switch (value.color) {
-            case ApuestasEstados.HIGH:
-              acc.high += 1
-              break
-            case ApuestasEstados.STAGE:
-              acc.stage += 1
-              break
-            case ApuestasEstados.LOW:
-              acc.low += 1
-              break
-          }
-
-          return acc
-        },
-        { total: 0, high: 0, stage: 0, low: 0 },
-      ),
-    }))
-  },
-  resetVelas: () => {
-    set({ velas: { total: 0, high: 0, low: 0, stage: 0 } })
   },
 }))
